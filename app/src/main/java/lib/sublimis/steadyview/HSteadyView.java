007d0adfaea2414d163850350ab5b1b47ec74eee
Sublimis <sublimis@gmail.com>
2024-12-19 15:34:51 +0100

    v2.0

---
+++ app/src/main/java/lib/sublimis/steadyview/HSteadyView.java
@@ -0,0 +1,203 @@
+/*
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package lib.sublimis.steadyview;
+
+import android.annotation.SuppressLint;
+import android.os.Build.VERSION;
+import android.os.Build.VERSION_CODES;
+import android.os.Bundle;
+import android.os.SystemClock;
+import android.view.View;
+import android.view.View.AccessibilityDelegate;
+import android.view.accessibility.AccessibilityNodeInfo;
+
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.atomic.AtomicReference;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+
+/**
+ * Interface to the <a href="https://play.google.com/store/apps/details?id=com.sublimis.steadyscreen">SteadyScreen</a> service
+ * which allows compatible applications to easily counteract small screen movements within their GUI.
+ * <p>
+ * Find out more at <a href="https://github.com/Sublimis/SteadyView/">https://github.com/Sublimis/SteadyView/</a>.
+ *
+ * @author Sublimis
+ * @version 2.0 (2024-11)
+ */
+@SuppressLint("ObsoleteSdkInt")
+public class HSteadyView
+{
+	protected static final long UndoTimeout = TimeUnit.SECONDS.toNanos(2);
+	protected static final long UndoCheckTimeout = TimeUnit.MILLISECONDS.toNanos(1000);
+	protected static long mActionTime = 0, mUndoCheckTime = 0;
+
+	public static void initSteadyView(@NonNull final ISteadyView steadyView)
+	{
+		if (VERSION.SDK_INT >= VERSION_CODES.LOLLIPOP && steadyView instanceof View)
+		{
+			final SteadyAccDelegate steadyAccDelegate = new SteadyAccDelegate()
+			{
+				@Override
+				public void onInitializeAccessibilityNodeInfo(@NonNull final View host, @NonNull final AccessibilityNodeInfo info)
+				{
+					super.onInitializeAccessibilityNodeInfo(host, info);
+
+					info.addAction(ISteadyView.STEADY_MARK);
+					info.setText(ISteadyView.STEADY_MARK.getLabel());
+
+					if (steadyView.isSteadyViewEnabled())
+					{
+						info.addAction(ISteadyView.STEADY_ACTION);
+					}
+
+					this.mNodeRef.set(info);
+				}
+			};
+
+			steadyView.setSteadyAccDelegate(steadyAccDelegate);
+			((View) steadyView).setAccessibilityDelegate(steadyAccDelegate);
+
+			((View) steadyView).setImportantForAccessibility(View.IMPORTANT_FOR_ACCESSIBILITY_YES);
+		}
+	}
+
+	public static boolean performSteadyViewAction(@NonNull final ISteadyView steadyView, final int action, @Nullable final Bundle arguments)
+	{
+		boolean retVal = false;
+
+		if (steadyView.isSteadyViewEnabled() && steadyView instanceof View)
+		{
+			if (arguments != null && isSteadyViewAction(action, arguments))
+			{
+				final int x = arguments.getInt(ISteadyView.ARG_MOVE_X, Integer.MIN_VALUE);
+				final int y = arguments.getInt(ISteadyView.ARG_MOVE_Y, Integer.MIN_VALUE);
+
+				if (x != Integer.MIN_VALUE)
+				{
+					((View) steadyView).setTranslationX(x);
+
+					retVal = true;
+				}
+
+				if (y != Integer.MIN_VALUE)
+				{
+					((View) steadyView).setTranslationY(y);
+
+					retVal = true;
+				}
+
+				manageUndo(steadyView);
+			}
+		}
+
+		return retVal;
+	}
+
+	public static void undoSteadyViewAction(@NonNull final ISteadyView steadyView)
+	{
+		if (steadyView instanceof View)
+		{
+			((View) steadyView).setTranslationX(0);
+			((View) steadyView).setTranslationY(0);
+		}
+	}
+
+	protected static void manageUndo(@NonNull final ISteadyView steadyView)
+	{
+		final long timestamp = SystemClock.elapsedRealtimeNanos();
+
+		if (isUndoCheckNeeded(timestamp))
+		{
+			((View) steadyView).postDelayed(new Runnable()
+			{
+				@Override
+				public void run()
+				{
+					if (isUndoNeeded())
+					{
+						undoSteadyViewAction(steadyView);
+					}
+				}
+			}, TimeUnit.NANOSECONDS.toMillis(UndoTimeout + UndoCheckTimeout) + 500);
+
+			mUndoCheckTime = timestamp;
+		}
+
+		mActionTime = timestamp;
+	}
+
+	protected static boolean isUndoNeeded()
+	{
+		final long timestamp = SystemClock.elapsedRealtimeNanos();
+
+		return timestamp - mActionTime > UndoTimeout;
+	}
+
+	protected static boolean isUndoCheckNeeded(final long timestamp)
+	{
+		return timestamp - mUndoCheckTime > UndoCheckTimeout;
+	}
+
+	public static boolean isSteadyViewAction(final int action, @Nullable final Bundle arguments)
+	{
+		boolean retVal = false;
+
+		if (VERSION.SDK_INT >= VERSION_CODES.LOLLIPOP)
+		{
+			if (action == ISteadyView.STEADY_ACTION.getId())
+			{
+				retVal = true;
+			}
+		}
+
+		return retVal;
+	}
+
+	public static void populateSteadyViewArguments(final Bundle arguments, final int x, final int y)
+	{
+		if (arguments != null)
+		{
+			arguments.putInt(ISteadyView.ARG_MOVE_X, x);
+			arguments.putInt(ISteadyView.ARG_MOVE_Y, y);
+		}
+	}
+
+	public static class SteadyAccDelegate extends AccessibilityDelegate
+	{
+		protected final AtomicReference<AccessibilityNodeInfo> mNodeRef = new AtomicReference<>(null);
+
+		public void enable()
+		{
+			final AccessibilityNodeInfo node = mNodeRef.get();
+
+			if (node != null)
+			{
+				node.addAction(ISteadyView.STEADY_ACTION);
+			}
+		}
+
+		public void disable()
+		{
+			final AccessibilityNodeInfo node = mNodeRef.get();
+
+			if (node != null)
+			{
+				node.removeAction(ISteadyView.STEADY_ACTION);
+			}
+		}
+	}
+}
---
+++ app/src/main/java/lib/sublimis/steadyview/ISteadyView.java
@@ -0,0 +1,123 @@
+/*
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package lib.sublimis.steadyview;
+
+import android.os.Bundle;
+import android.view.accessibility.AccessibilityNodeInfo.AccessibilityAction;
+
+import java.util.concurrent.atomic.AtomicBoolean;
+
+import androidx.annotation.Nullable;
+import lib.sublimis.steadyview.HSteadyView.SteadyAccDelegate;
+
+/**
+ * Interface to the <a href="https://play.google.com/store/apps/details?id=com.sublimis.steadyscreen">SteadyScreen</a> service
+ * which allows compatible applications to easily counteract small screen movements within their GUI.
+ * <p>
+ * Find out more at <a href="https://github.com/Sublimis/SteadyView/">https://github.com/Sublimis/SteadyView/</a>.
+ *
+ * @author Sublimis
+ * @version 2.0 (2024-11)
+ */
+public interface ISteadyView
+{
+	AccessibilityAction STEADY_MARK = new AccessibilityAction(1000573491, "(SteadyView)");
+	AccessibilityAction STEADY_ACTION = new AccessibilityAction(1000573492, "(SteadyViewAction)");
+	String ARG_MOVE_X = "lib.steadyview.MOVE_X";
+	String ARG_MOVE_Y = "lib.steadyview.MOVE_Y";
+	AtomicBoolean mIsEnabled = new AtomicBoolean(true);
+
+	/**
+	 * Get the SteadyView feature global enabled state.
+	 *
+	 * @return true if SteadyView feature is enabled globally
+	 */
+	default boolean isSteadyViewEnabled()
+	{
+		return mIsEnabled.get();
+	}
+
+	/**
+	 * Set the SteadyView feature global enabled state.
+	 *
+	 * @param enabled true to enable the SteadyView feature globally, false to disable
+	 * @return true if SteadyView feature enabled state was changed as a result of this call
+	 */
+	default boolean setSteadyViewEnabled(final boolean enabled)
+	{
+		final boolean retVal = mIsEnabled.compareAndSet(!enabled, enabled);
+
+		if (retVal)
+		{
+			final SteadyAccDelegate steadyAccDelegate = getSteadyAccDelegate();
+
+			if (enabled)
+			{
+				if (steadyAccDelegate != null)
+				{
+					steadyAccDelegate.enable();
+				}
+			}
+			else
+			{
+				if (steadyAccDelegate != null)
+				{
+					steadyAccDelegate.disable();
+				}
+
+				undoSteadyViewAction();
+			}
+		}
+
+		return retVal;
+	}
+
+	/**
+	 * Init the SteadyView feature. This should be called from the custom view constructor.
+	 */
+	default void initSteadyView()
+	{
+		HSteadyView.initSteadyView(this);
+	}
+
+	/**
+	 * Perform the SteadyView feature accessibility action if requested.
+	 *
+	 * @param action The accessibility action to perform.
+	 * @param arguments Optional accessibility action arguments.
+	 * @return true if the SteadyView action was performed, false if SteadyView feature is disabled or the action was not recognized.
+	 */
+	default boolean performSteadyViewAction(final int action, @Nullable final Bundle arguments)
+	{
+		return HSteadyView.performSteadyViewAction(this, action, arguments);
+	}
+
+	/**
+	 * Undo all side effects of previous SteadyView feature actions.
+	 */
+	default void undoSteadyViewAction()
+	{
+		HSteadyView.undoSteadyViewAction(this);
+	}
+
+	default SteadyAccDelegate getSteadyAccDelegate()
+	{
+		return null;
+	}
+
+	default void setSteadyAccDelegate(SteadyAccDelegate steadyAccDelegate)
+	{
+	}
+}
